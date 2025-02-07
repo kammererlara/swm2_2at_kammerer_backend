809dec186524c517616913553e5f0ac7420ee44a
Lara Kammerer <lara.kammerer@gmail.com>
2025-02-07 22:37:56 +0100

    feat: fix #11
    UserController added
    getUserByName in UserService and UserRepository renamed to getUserByFirstnameAndLastname
    Unittests and Integrationtests added

---
+++ src/main/java/hs_burgenland/weather/controller/UserController.java
@@ -0,0 +1,59 @@
+package hs_burgenland.weather.controller;
+
+import hs_burgenland.weather.entities.User;
+import hs_burgenland.weather.exceptions.EntityAlreadyExistingException;
+import hs_burgenland.weather.exceptions.EntityNotFoundException;
+import hs_burgenland.weather.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody final User user) {
+        try {
+            return ResponseEntity.ok(userService.createUser(user.getFirstname(), user.getLastname()));
+        } catch (EntityAlreadyExistingException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllUsers() {
+        try {
+            return ResponseEntity.ok(userService.getAllUsers());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable final int id) {
+        try {
+            return ResponseEntity.ok(userService.getUserById(id));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable final int id) {
+        try {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+}
