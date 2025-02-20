4dcd1e0aeebefe4a9d487e1e422b0e9994b1cca3
Lara Kammerer <lara.kammerer@gmail.com>
2025-02-20 19:16:49 +0100

    feat: fix #45
    Adjustments for e2e tests

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,8 @@
 INSERT INTO users (id, firstname, lastname) VALUES (0, 'Jane', 'Doe') ON CONFLICT (id) DO NOTHING;
+
+-- Testdata
+INSERT INTO locations (id, name, latitude, longitude, elevation, icao)
+    VALUES (1, 'Vienna,Austria', 48.2082, 16.3738, 151, 'LOWW')
+        ON CONFLICT (id) DO NOTHING;
+INSERT INTO favorites (name, user_id, location_id)
+    VALUES ('Wien', 0, 1);
